17295cc627bdf55f7e476e533320597fa07e41fb
Maria Sol R. Gomez <48770634+Sol-gomez@users.noreply.github.com>
2019-09-08 01:59:37 -0300

    Create faq.php

---
+++ faq.php
@@ -0,0 +1,37 @@
+<div class="jumbotron ">
+  <div class="container-fluid">
+    <h1 class="display-4">Preguntas Frecuentes</h1>
+    <hr class="my-4">
+    <p class="lead">1: ¿Como Permuto?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus animi laborum maxime consectetur magnam! Tempora velit labore suscipit atque, dicta, amet itaque, maxime possimus adipisci sunt error animi consectetur repudiandae?
+   <hr class="my-4">
+    <p class="lead">2:¿Como me registro? ¿Como Ingreso?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat architecto perferendis omnis iure eum voluptate laboriosam explicabo sapiente impedit laudantium autem similique distinctio vitae repellat harum vero, ea molestias! Quo.</p>
+    <hr class="my-4">
+    <p class="lead">3:Subir mis productos a la web</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat architecto perferendis omnis iure eum voluptate laboriosam explicabo sapiente impedit laudantium autem similique distinctio vitae repellat harum vero, ea molestias! Quo.</p>
+    <hr class="my-4">
+    <p class="lead">4:Ingresar a mi cuenta</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat architecto perferendis omnis iure eum voluptate laboriosam explicabo sapiente impedit laudantium autem similique distinctio vitae repellat harum vero, ea molestias! Quo.</p>
+    <hr class="my-4">
+    <p class="lead">5:Modalidades de Pago</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat architecto perferendis omnis iure eum voluptate laboriosam explicabo sapiente impedit laudantium autem similique distinctio vitae repellat harum vero, ea molestias! Quo.</p>
+    <hr class="my-4">
+    <p class="lead">6:Todo lo que tenes que saber sobre la PERMUTA, Permuta+dinero</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat architecto perferendis omnis iure eum voluptate laboriosam explicabo sapiente impedit laudantium autem similique distinctio vitae repellat harum vero, ea molestias! Quo.</p>
+    <hr class="my-4">
+    <p class="lead">7:Politicas de Reembolsos y Devoluciones</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat architecto perferendis omnis iure eum voluptate laboriosam explicabo sapiente impedit laudantium autem similique distinctio vitae repellat harum vero, ea molestias! Quo.</p>
+    <hr class="my-4">
+    <p class="lead">8:Politicas de Privacidad</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat architecto perferendis omnis iure eum voluptate laboriosam explicabo sapiente impedit laudantium autem similique distinctio vitae repellat harum vero, ea molestias! Quo.</p>
+    <hr class="my-4">
+    <p class="lead">9:Ayuda</p>
+    <p>Ponte en contactato con nuestro Servicio de Atencion al Cliente 24hs atraves del numero 444000999 o simplemente mandando un mail a user@permutafacil.com</p>
+    
+  </div>
+</div>
+    
+  </div>
+</div>
+
