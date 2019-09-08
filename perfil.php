08faa12f6ccc71818ae1a0ae712e011d7c831f60
Maria Sol R. Gomez <48770634+Sol-gomez@users.noreply.github.com>
2019-09-08 01:57:35 -0300

    Create perfil.php

---
+++ perfil.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/perfil.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    
+    <title>Inicio de sesion</title>
+</head>
+<body>
+       <div class="container">
+         <header>
+           <?php include_once "encabezado.php" ?>
+         </header>
+
+         <!-- Perfil -->
+        <div class="card" style="width: 18rem;">
+            <img src="img/fotoPerfil.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                  <h5 class="card-title">David Albornoz</h5>
+                  <p class="card-text">Busco permutar mis articulos por articulos de Tecnologia en general, soy de zona norte. Prov. Buenos Aires.</p>
+                  <a href="#" class="btn btn-primary">Enviar Mensaje</a>
+            </div>
+        </div>
+
+            <div class="row">
+               <div class="col-sm-4">
+                  <div class="card">
+                        <img src="img/shoes-1911527_1920.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Special title treatment</h5>
+                      <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col-sm-4">
+                  <div class="card">
+                        <img src="img/nike.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Special title treatment</h5>
+                      <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                  </div>
+                </div>
+                  <div class="col-sm-4">
+                    <div class="card">
+                            <img src="img/skateboards.jpg" class="card-img-top" alt="60px">
+                      <div class="card-body">
+                        <h5 class="card-title">Special title treatment</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                      </div>
+                    </div>
+                </div>
+              </div>
+</div>            
+</body>
+</html>
