0bb87611500ae00993c0a775e592183321a65a96
Maria Sol R. Gomez <48770634+Sol-gomez@users.noreply.github.com>
2019-09-08 01:58:16 -0300

    Create encabezado.php

---
+++ encabezado.php
@@ -0,0 +1,23 @@
+<div class="pos-f-t">
+            <div class=" dropdown collapse" id="navbarToggleExternalContent">
+                        <div class="bg-dark p-4">
+                          <a class="dropdown-item" href="#home_s">Home</a>
+                          <a class="dropdown-item" href="#productos_s">Productos</a>
+                          <a class="dropdown-item" href="#faq_s">F.A.Q</a>
+                          <a class="dropdown-item" href="#registrarse_s">Registrarse</a>
+                          <a class="dropdown-item" href="#ingresar_s">Ingresar</a>
+                          <a class="dropdown-item" href="#tuPerfil_s">Tu Perfil</a>
+                          <a class="dropdown-item" href="#nosotros_s">Sobre Nosotros</a>
+                        </div>
+            </div>
+          <nav class="navbar navbar-dark bg-dark">
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <!--Boton Buscar -->
+              <form class="form-inline my-2 my-lg-0">
+                  <input class="control form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Buscar" >
+                  <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Ir</button>
+                </form>
+          </nav>   
+      </div>
