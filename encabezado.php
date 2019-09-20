ffa173c854388e704fc202e4ab49b7d97396df1a
Maria Sol R. Gomez <48770634+Sol-gomez@users.noreply.github.com>
2019-09-20 18:51:14 -0300

    Update encabezado.php

--- encabezado.php
+++ encabezado.php
@@ -1,26 +1,10 @@
-<div class="pos-f-t">
-            <div class=" dropdown collapse" id="navbarToggleExternalContent">
-                        <div class="bg-dark p-4">
-                          <a class="dropdown-item" href="#home_s">Home</a>
-                          <a class="dropdown-item" href="#productos_s">Productos</a>
-                          <a class="dropdown-item" href="#faq_s">F.A.Q</a>
-                          <a class="dropdown-item" href="../tpIntegrador/registro.php">Registrarse</a>
-                          <a class="dropdown-item" href="../tpIntegrador/inicioSesion.php">Ingresar</a>
-                          <a class="dropdown-item" href="../tpIntegrador/perfil.php">Tu Perfil</a>
-                          <a class="dropdown-item" href="#nosotros_s">Sobre Nosotros</a> 
-                        
-                        </div>
-            </div>
-        
-          <nav class="navbar navbar-dark bg-dark">
-              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-                <h6>Categorias</h6>
-              </button>
-              <!--Boton Buscar -->
-              <form class="form-inline my-2 my-lg-0">
-                  <input class="control form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Buscar" >
-                  <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Ir</button>
-                </form>
-          </nav>   
-      </div>
+    <nav class="navbar navbar-expand nav-fill" style="background-color: #e3f2fd;" >
+ 
+  <a class="nav-item nav-link active" href="#">Home</a>
+  <a class="nav-item nav-link active" href="#">Productos</a>
+  <a class="nav-item nav-link active" href="#">FAQ</a>
+  <a class="nav-item nav-link active" href="#" >Sobre Nosotros</a>
+  <a class="nav-item nav-link active" href="#" >Ingresar</a>
+  <a class="nav-item nav-link active" href="#" >Tu Perfil</a>
+
+</nav>
