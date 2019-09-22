29fea3ec29b948ff6072f8b589a8c193e9bb4180
Maria Sol R. Gomez <48770634+Sol-gomez@users.noreply.github.com>
2019-09-22 02:22:18 -0300

    Update productos.php

--- productos.php
+++ productos.php
@@ -1,83 +1,317 @@
 <!DOCTYPE html>
+
 <html lang="en">
+
   <head>
+
     <!-- Required meta tags -->
+
     <meta charset="utf-8">
+
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+
+
     <!-- Bootstrap CSS -->
+
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
     <link rel="stylesheet" href="css/productos.css">
+
     <title>Productos</title>
+
   </head>
 
-  <body >
+
+
+  <body class="bg-info">
+
     <div class="container-fluid">
-    <header>
-        <?php include_once "encabezado.php"; ?>
-    </header>
+
+    <<header class="navbar-nav">
+
+        <div class="pos-f-t">
+
+          <div class=" dropdown collapse" id="navbarToggleExternalContent">
+
+                      <div class="bg-dark p-4">
+
+                        <a class="dropdown-item" href="#home_s">Home</a>
+
+                        <a class="dropdown-item" href="#productos_s">Productos</a>
+
+                        <a class="dropdown-item" href="#faq_s">F.A.Q</a>
+
+                        <a class="dropdown-item" href="../tpIntegrador/registro.php">Registrarse</a>
+
+                        <a class="dropdown-item" href="../tpIntegrador/inicioSesion.php">Ingresar</a>
+
+                        <a class="dropdown-item" href="../tpIntegrador/perfil.php">Tu Perfil</a>
+
+                        <a class="dropdown-item" href="#nosotros_s">Sobre Nosotros</a> 
+
+                      
+
+                      </div>
+
+          </div>
+
     <br>
-      <section class="header">
-            <article class="card text-center">
-                    <article class="card-header">
-                      <ul class="nav nav-tabs card-header-tabs">
-                        <li class="nav-item">
-                          <a class="nav-link active" href="#">Productos</a>
-                        </li>
-                        <li class="nav-item">
-                          <a class="nav-link" href="#">Recomendados para ti</a>
-                        </li>
-                       
-                      </ul>
+
+    </section>
+
+      <section class="card">
+
+          <article class="card text-center mx-auto d-block">
+
+            <ul class="list-group list-group-horizontal">
+
+              <a href="#">
+
+                <li class="list-group-item flex-fill">Recientemente agregados</li>
+
+              </a>
+
+              <a href="#">
+
+                <li class="list-group-item flex-fill">Ofertas!</li>
+
+              </a>
+
+              <a href="#"><li class="list-group-item flex-fill">Tus preferencias</li>
+
+            </a>
+
+            </ul>
+
+          </article>
+
+      <section class="card-deck">  
+
+            <article class="card-body align-items-center border-0">
+
+                            <article class="_cajas col  card float-left" style="width: 17rem;">
+
+                                    <img src="img/celular.webp" class="card-img-top img-fluid m-1" alt="...">
+
+                                    <article class="card-body">
+
+                                      <h5 class="card-title">Nombre Producto</h5>
+
+                                      <p class="card-text">Un breve desarrollo del producto.</p>
+
+                                      <a href="#" class="btn btn-primary">Ofrecer</a>
+
+                                      <div class="card-footer m-1">
+
+                                        <small class="text-muted">Publicado el xx de xx del xx</small>
+
+                                      </div>
+
+                                    </article>
+
+                                </article>
+
+                            <article class="_cajas col  card float-left" style="width: 17rem;">
+
+                                    <img src="img/mesa.jpg" class="card-img-top img-fluid m-1" alt="...">
+
+                                    <article class="card-body">
+
+                                       <h5 class="card-title">Nombre Producto</h5>
+
+                                       <p class="card-text">Un breve desarrollo del producto.</p>
+
+                                       <a href="#" class="btn btn-primary">Ofrecer</a>
+
+                                       <div class="card-footer m-1">
+
+                                        <small class="text-muted">Publicado el dd de mm del aa</small>
+
+                                      </div>
+
+                                    </article>
+
+                                </article>
+
+                             <article class="_cajas col  card float-left" style="width: 17rem;">
+
+                                    <img src="img/ropa1.jpg" class="card-img-top img-fluid m-1" alt="...">
+
+                                    <article class="card-body">
+
+                                        <h5 class="card-title">Nombre Producto</h5>
+
+                                        <p class="card-text">Un breve desarrollo del producto.</p>
+
+                                        <a href="#" class="btn btn-primary">Ofrecer</a>
+
+                                        <div class="card-footer m-1">
+
+                                          <small class="text-muted">Publicado el dd de mm del aa</small>
+
+                                        </div>
+
+                                    </article>
+
+                                </article>
+
+                              <article class="_cajas col  card float-left" style="width: 17rem;">
+
+                                    <img src="img/ropa2.jpg" class="card-img-top img-fluid m-1" alt="...">
+
+                                    <article class="card-body">
+
+                                        <h5 class="card-title">Nombre Producto</h5>
+
+                                        <p class="card-text">Un breve desarrollo del producto.</p>
+
+                                        <a href="#" class="btn btn-primary">Ofrecer</a>
+
+                                        <div class="card-footer m-1">
+
+                                          <small class="text-muted">Publicado el dd de mm del aa</small>
+
+                                        </div>
+
+                                    </article>
+
+                                </article>
+
+                              <article class="_cajas col  card float-left" style="width: 17rem;">
+
+                                    <img src="img/ropa3.jpg" class="card-img-top img-fluid m-1" alt="...">
+
+                                    <article class="card-body">
+
+                                        <h5 class="card-title">Nombre Producto</h5>
+
+                                        <p class="card-text">Un breve desarrollo del producto.</p>
+
+                                        <a href="#" class="btn btn-primary">Ofrecer</a>
+
+                                        <div class="card-footer m-1">
+
+                                          <small class="text-muted">Publicado el dd de mm del aa</small>
+
+                                        </div>
+
+                                    </article>
+
+                                </article>
+
+                                <article class="_cajas col  card float-left" style="width: 17rem;">
+
+                                    <img src="img/ropa3.jpg" class="card-img-top img-fluid m-1" alt="...">
+
+                                    <article class="card-body">
+
+                                        <h5 class="card-title">Nombre Producto</h5>
+
+                                        <p class="card-text">Un breve desarrollo del producto.</p>
+
+                                        <a href="#" class="btn btn-primary">Ofrecer</a>
+
+                                        <div class="card-footer m-1">
+
+                                          <small class="text-muted">Publicado el dd de mm del aa</small>
+
+                                        </div>
+
+                                    </article>
+
+                                </article>
+
+                                <article class="_cajas col  card float-left" style="width: 17rem;">
+
+                                    <img src="img/ropa3.jpg" class="card-img-top img-fluid m-1" alt="...">
+
+                                    <article class="card-body">
+
+                                        <h5 class="card-title">Nombre Producto</h5>
+
+                                        <p class="card-text">Un breve desarrollo del producto.</p>
+
+                                        <a href="#" class="btn btn-primary">Ofrecer</a>
+
+                                        <div class="card-footer m-1">
+
+                                          <small class="text-muted">Publicado el dd de mm del aa</small>
+
+                                        </div>
+
+                                    </article>
+
+                                </article>
+
+                                <article class="_cajas col  card float-left" style="width: 17rem;">
+
+                                    <img src="img/ropa3.jpg" class="card-img-top img-fluid m-1" alt="...">
+
+                                    <article class="card-body">
+
+                                        <h5 class="card-title">Nombre Producto</h5>
+
+                                        <p class="card-text">Un breve desarrollo del producto.</p>
+
+                                        <a href="#" class="btn btn-primary">Ofrecer</a>
+
+                                        <div class="card-footer m-1">
+
+                                          <small class="text-muted">Publicado el dd de mm del aa</small>
+
+                                        </div>
+
+                                    </article>
+
+                                </article>
+
+                      </section>
+
                     </article>
-                    <article class="card-body" >
-                            <article class="m-4 card float-left _cajas" style="width: 18rem;">
-                                    <img src="img/cel.webp" class="card-img-top" alt="...">
-                                    <article class="card-body">
-                                      <h5 class="card-title">Card title</h5>
-                                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                      <a href="#" class="btn btn-primary">Go somewhere</a>
-                                    </article>
-                                </article>
-                            <article class="m-4 card float-left _cajas" style="width: 18rem;">
-                                    <img src="img/mesa.jpg" class="card-img-top" alt="...">
-                                    <article class="card-body">
-                                       <h5 class="card-title">Card title</h5>
-                                       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                       <a href="#" class="btn btn-primary">Go somewhere</a>
-                                    </article>
-                                </article>
-                             <article class="m-4 card float-left _cajas" style="width: 18rem;">
-                                    <img src="img/ropa.jpg" class="card-img-top" alt="...">
-                                    <article class="card-body">
-                                        <h5 class="card-title">Card title</h5>
-                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                                    </article>
-                                </article>
-                               
-                    </article>
+
             </article>
-      </section>
-      <section> 
-            <nav aria-label="Page navigation example">
-                    <ul class="pagination justify-content-center">
-                      <li class="page-item disabled">
-                        <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Anterior</a>
-                      </li>
-                      <li class="page-item"><a class="page-link" href="#">1</a></li>
-                      <li class="page-item"><a class="page-link" href="#">2</a></li>
-                      <li class="page-item"><a class="page-link" href="#">3</a></li>
-                      <li class="page-item">
-                        <a class="page-link" href="#">Siguiente</a>
-                      </li>
-                    </ul>
-                  </nav>
+
       </section>
 
+            <nav aria-label="Page navigation example">
+
+                    <ul class="pagination justify-content-center">
+
+                      <li class="page-item">
+
+                        <a class="page-link" href="#">Anterior</a>
+
+                      </li>
+
+                      <li class="page-item"><a class="page-link" href="#">1</a></li>
+
+                      <li class="page-item"><a class="page-link" href="#">2</a></li>
+
+                      <li class="page-item"><a class="page-link" href="#">3</a></li>
+
+                      <li class="page-item">
+
+                        <a class="page-link" href="#">Siguiente</a>
+
+                      </li>
+
+                    </ul>
+
+                  </nav>
+
+      </section>
+
+
+
     </div>
+
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
   </body>
+
 </html>
