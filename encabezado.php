596b1b8bbebd3d1ce066425a198aa1cf05d75918
TomasGranado <54644361+TomasGranado@users.noreply.github.com>
2019-10-04 17:01:16 -0300

    Update encabezado.php

--- encabezado.php
+++ encabezado.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+require_once('controladores/funciones.php')
+?>
 <nav class="navbar navbar-expand nav-fill" style="background-color: #e3f2fd;" >
  
   <a class="nav-item nav-link active" href="index.php">Home</a>
@@ -6,5 +10,6 @@
   <a class="nav-item nav-link active" href="#sobreNosotros" >Sobre Nosotros</a>
   <a class="nav-item nav-link active" href="inicioSesion.php" >Ingresar</a>
   <a class="nav-item nav-link active" href="perfil.php" >Tu Perfil</a>
+  <a class="dropdown-item" href="logOut.php">Cerrar sesion</a> 
 
 </nav>
