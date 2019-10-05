95cda6a37d55730982f1ef0a46a2627593acc3b3
TomasGranado <54644361+TomasGranado@users.noreply.github.com>
2019-10-04 21:18:11 -0300

    Update inicioSesion.php

--- inicioSesion.php
+++ inicioSesion.php
@@ -57,7 +57,7 @@ if($_POST){
 				</div>
 			</div>
 			<div class="card-body">
-				<form>
+				<form method= "POST">
 					<div class="input-group form-group">
 						<div class="input-group-prepend">
 							<span class="input-group-text"><i class="fas fa-user"></i></span>
