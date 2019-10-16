af0adb5679900fc8f2643375cc237ef0201d9019
David97-bit <54641861+David97-bit@users.noreply.github.com>
2019-10-16 20:03:16 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+require_once('encabezado.php');
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,29 +15,46 @@
     <div class="container-fluid">
           
       <header>
-        <?php include_once "encabezado.php"; ?>
+      <?php cargarNavbarIndex();?> 
       </header>
       
       <div class="contenedorEntrada-S">
-          <img src="img/fondoPantalla.jpg" style="width: 100%;height: 50%;"alt="foto"> 
-          <h1 class="textoEncima">Comienza a intercambiar tus objetos</h1>
-          <a class="botonEncima1 btn btn-primary btn-lg" href="#" role="button">Registrarse</a>
-          <a class=" botonEncima btn btn-primary btn-lg" href="#" role="button">Iniciar Sesion</a>
+
+
+      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+    <img class="d-block w-100" src="img/index1.jpg" style="width: 100%;height: 570px;" alt="First slide">
+    </div>
+    <div class="carousel-item">
+    <img class="d-block w-100" src="img/index2.png" style="width: 100%;height: 570px;" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+    <img class="d-block w-100" src="img/index3.jpg" style="width: 100%;height: 570px;" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+            
+
+        <!--  <img src="img/client-1.png" style="width: 100%;height: 50%;"alt="foto"> -->
+        <marquee direction="left" width="100%" bgcolor="black" scrollamount="3"><font size="5" color="white"> Comienza a intercambiar tus productos </marquee>
+          <a class="botonEncima1 btn btn-primary btn-lg" href="formulario.php" role="button">Registrarse</a>
+          <a class=" botonEncima btn btn-primary btn-lg" href="inicioSesion.php" role="button">Iniciar Sesion</a>
       </div>
       <br>
-       
-       <section >
-           <?php include_once "productos.php" ?>
-        </section>
-
-  
-    <section id="faq_s">
-        <?php include_once "faq.php"; ?>
-    </section>
-
-    <footer>
-      <?php include_once "pieDePag.php" ?>
-    </footer>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
